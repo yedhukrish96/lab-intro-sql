6339744d218cfaff75fb86fdda9246e96377da0a
Yedhu Krishnan <yedhukrish96@gmail.com>
2023-02-28 17:41:05 +0100

    Lab 1 solutions sql

---
+++ Lab1-Solutions.sql
@@ -0,0 +1,20 @@
+use sakila;
+
+SELECT * from actor;
+
+SELECT * from film;
+
+SELECT * from customer;
+
+SELECT title from film;
+
+SELECT name AS language
+FROM language;
+
+select count(store_id) from store;
+
+select count(staff_id) from staff;
+
+select first_name from staff;
+
+
